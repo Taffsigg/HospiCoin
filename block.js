4c4076c27b607ffe3bb6b3ba2df0ac1fae1a48b2
Vincent R <vincent.renotte@gmail.com>
2019-06-24 16:36:28 +0800

    Add createBlock using crypto-js

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+//Import the sha-256 function from module crypto-js
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data, validator, signature) {
         //the time of creation of block in milliseconds
@@ -23,5 +26,31 @@ class Block {
             Data      : ${this.data}
             Validator : ${this.validator}
             Signature : ${this.signature}`;
-        }
+    }
+
+    //This function will create a new block with dummy values.
+    //Static because we don't want to create an instance to call this fct
+    static genesis() {
+        return new this(`genesis time`, "----", "genesis-hash", []);
+    }
+
+
+    static hash(timestamp,lastHash,data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+
+    //Arguments are :
+    //  the lastBlock, 
+    //  the data
+    // based on these two items it generates a new block
+    static createBlock(lastBlock, data) {
+        let hash;
+        let timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        hash = Block.hash(timestamp, lastHash, data);
+    
+        return new this(timestamp, lastHash, hash, data);
+      }
+
   }
+
