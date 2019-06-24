8228a3cb05f1a0fa00f9c471c4951683fda0cd26
Vincent R <vincent.renotte@gmail.com>
2019-06-24 23:01:03 +0800

    Create transaction class

--- chain-util.js
+++ chain-util.js
@@ -1,11 +1,29 @@
 //hold the functions related to cryptocurrency
 const EDDSA = require("elliptic").eddsa;
 const eddsa = new EDDSA("ed25519");
+//To generate these unique numbers
+const uuidV1 = require('uuid/v1');
 
 //function to generate key pair
 class ChainUtil {
     static genKeyPair(secret) {
       return eddsa.keyFromSecret(secret);
     }
+
+    //function to generate ids.
+    static id(){
+        return uuidV1();
+    }
+}
+module.exports = ChainUtil;
+
+class Transaction {
+    constructor() {
+      this.id = ChainUtil.id();
+      this.type = null;
+      this.input = null;
+      this.output = null;
+    }
   }
-  module.exports = ChainUtil;
+  
+  module.exports = Transaction;
