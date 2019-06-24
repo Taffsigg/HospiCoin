65a65ca45e432adafd68161356c7dc0a541b0953
Vincent R <vincent.renotte@gmail.com>
2019-06-24 23:29:03 +0800

    add transaction pool

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,26 @@
+//The transaction-pool will be an object updated in real time that contains all the new transactions submitted by all miners in the network.
+const Transaction = require("./transaction");
+const { TRANSACTION_THRESHOLD } = require("../config");
+
+class TransactionPool {
+    constructor() {
+        this.transactions = [];
+    }
+
+    // push transactions in the list
+    addTransaction(transaction) {
+        this.transactions.push(transaction);
+    }
+
+    validTransactions() {
+        return this.transactions.filter(transaction => {
+            if (!Transaction.verifyTransaction(transaction)) {
+                console.log(`Invalid signature from ${transaction.data.from}`);
+                return;
+            }
+    
+        return transaction;
+        });
+      }
+}
+module.exports = TransactionPool;
