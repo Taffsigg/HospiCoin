7bab7ab8d2cec155dc2d37d7217a17ae98d479e5
Vincent R <vincent.renotte@gmail.com>
2019-06-25 11:33:53 +0800

    add account model

---
+++ blockchain/account.js
@@ -0,0 +1,47 @@
+//object in our application to create an account model.
+
+class Account {
+    constructor() {
+      this.addresses = [];
+      this.balance = {};
+    }
+  
+    initialize(address) {
+      if (this.balance[address] == undefined) {
+        this.balance[address] = 0;
+        this.addresses.push(address);
+      }
+    }
+  
+    // utilizes the increment and decrement functions to emulate transfer of value
+    transfer(from, to, amount) {
+      this.initialize(from);
+      this.initialize(to);
+      this.increment(to, amount);
+      this.decrement(from, amount);
+    }
+  
+    increment(to, amount) {
+      this.balance[to] += amount;
+    }
+  
+    decrement(from, amount) {
+      this.balance[from] -= amount;
+    }
+  
+    //returns the value of a given key. 
+    getBalance(address) {
+      this.initialize(address);
+      return this.balance[address];
+    }
+  
+    update(transaction) {
+      let amount = transaction.output.amount;
+      let from = transaction.input.from;
+      let to = transaction.output.to;
+      this.transfer(from, to, amount);
+    }
+  
+  }
+  
+  module.exports = Account;
